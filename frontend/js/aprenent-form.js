691e797b4b55adf73edbbf5c85f08dcbc8c03001
antoniCLL <salvamonic@hotmail.com>
2026-04-20 21:50:52 +0200

    Implementada la pàgina aprenent.html i adaptada a responsive

--- frontend/js/aprenent-form.js
+++ frontend/js/aprenent-form.js
@@ -3,6 +3,6 @@ const estatAprenent = document.getElementById("estat_aprenent");
 
 formulariAprenent.addEventListener("submit", (esdeveniment) => {
   esdeveniment.preventDefault();
-  estatAprenent.textContent = "Formulari d'aprenent enviat correctament (simulacio).";
+  estatAprenent.textContent = "Formulari d'aprenent enviat correctament (simulació).";
   formulariAprenent.reset();
 });
